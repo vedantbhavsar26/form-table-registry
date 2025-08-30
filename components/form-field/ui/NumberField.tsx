0ebd93146bad5c1d5aab6e4dad6cdf91382493a0
vedantbhavsar <vedantbhavsar67@gmail.com>
2025-08-30 16:11:33 +0530

    Add control buttons to NumberField and integrate react-hook-form in Home page

--- components/form-field/ui/NumberField.tsx
+++ components/form-field/ui/NumberField.tsx
@@ -1,7 +1,50 @@
 import React from 'react';
 import { Input } from '@/components/ui/input';
 import { BaseFieldProps } from '@/lib/form-field/form-field';
+import { cn } from '@/lib/form-field/utils';
+import { Button } from '@/components/ui/button';
+import { MinusIcon, PlusIcon } from 'lucide-react';
 
-export const NumberField = (field: BaseFieldProps) => {
-  return <Input {...field} type='number' />;
+export const NumberField = ({
+  withControls = false,
+  ...field
+}: BaseFieldProps & {
+  withControls?: boolean;
+}) => {
+  return (
+    <div className='relative z-10 flex items-center rounded-md shadow-xs'>
+      {withControls && (
+        <Button
+          variant={'ghost'}
+          onClick={() => field.onChange(field.value - 1)}
+          size={'sm'}
+          className='text-muted-foreground  absolute inset-y-0 start-0 mr-2 flex h-full items-center justify-center ps-3 text-sm'
+        >
+          <MinusIcon />
+        </Button>
+      )}
+      <Input
+        {...field}
+        type='number'
+        className={cn({
+          '-me-px h-full min-h-9 rounded-e-none ps-10 shadow-none text-center ': withControls,
+        })}
+      />
+      {withControls && (
+        <Button
+          variant={'ghost'}
+          size={'sm'}
+          onClick={() => field.onChange(field.value + 1)}
+          className={cn(
+            'text-muted-foreground  absolute inset-y-0 end-0  flex h-full items-center justify-center  text-sm',
+            {
+              'opacity-50': field.disabled,
+            },
+          )}
+        >
+          <PlusIcon />
+        </Button>
+      )}
+    </div>
+  );
 };
